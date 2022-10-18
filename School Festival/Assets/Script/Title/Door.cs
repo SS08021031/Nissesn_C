25ba6b21a0cc98a938d5b5ef5ff0445e1c76af02
Sugi <21cr0117@jec.ac.jp>
2022-10-18 12:14:40 +0900

    タイトル：扉

---
+++ School Festival/Assets/Script/Title/Door.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Vector3 pos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        transform.Translate(0, 0,2f, 0);
+
+    }
+}
